85933fb40ff9ca3e57eefaa893e770be3510a574
Mike Lewis <mikelikespie@gmail.com>
2012-02-27 12:13:01 -0800

    Initial Commit

---
+++ PonyDebugger/PonyDebugger.h
@@ -0,0 +1,13 @@
+//
+//  PonyDebugger.h
+//  PonyDebugger
+//
+//  Created by Mike Lewis on 2/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PonyDebugger : NSObject
+
+@end
---
+++ PonyDebugger/PonyDebugger.m
@@ -0,0 +1,13 @@
+//
+//  PonyDebugger.m
+//  PonyDebugger
+//
+//  Created by Mike Lewis on 2/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PonyDebugger.h"
+
+@implementation PonyDebugger
+
+@end
