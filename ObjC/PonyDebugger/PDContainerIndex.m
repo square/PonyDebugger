2b5dec1c1b069b9e4799aabe84baddbc621bfa5a
Wen-Hao Lue <me@wenhaolue.com>
2013-04-28 15:59:12 -0700

    Update comments.

--- ObjC/PonyDebugger/PDContainerIndex.m
+++ ObjC/PonyDebugger/PDContainerIndex.m
@@ -4,6 +4,9 @@
 //
 //  Created by Wen-Hao Lue on 2013-04-28.
 //
+//  Licensed to Square, Inc. under one or more contributor license agreements.
+//  See the LICENSE file distributed with this work for the terms under
+//  which Square, Inc. licenses this file to you.
 //
 
 #import "PDContainerIndex.h"
